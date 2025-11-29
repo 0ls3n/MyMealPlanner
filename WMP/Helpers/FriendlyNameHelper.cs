852943b9d27463cc10b0c0140fa55ca2d5c9ef27
Rasmus Olsen <rasmus782@gmail.com>
2025-11-29 23:28:02 +0100

    Added profile pictures for the users which didn't provide any image, and made some widgets for the dashboard to show the recipe's for today.

--- WMP/Helpers/FriendlyNameHelper.cs
+++ WMP/Helpers/FriendlyNameHelper.cs
@@ -8,4 +8,14 @@ public class FriendlyNameHelper
 
         return result;
     }
+    
+    public static string GetNameCredentials(string name)
+    {
+        var split = name.Split("_");
+
+        char[] firstSideCharacters = split[0].ToArray();
+        char[] lastSideCharacters = split[split.Length - 1].ToArray();
+
+        return $"{firstSideCharacters[0]}{lastSideCharacters[0]}".ToUpper();
+    }
 }
